9d043ddae7636e6eb5383aee0c438fc3632c0f6a
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2023-01-06 14:39:03 -0300

    tercer commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "include/Fecha.h"
 #include "include/Estudiante.h"
+#include "include/Examen.h"
 
 using namespace std;
 
 int main()
 {
-
-
+listar();
 
     return 0;
 }
